38558420d869977225c9d99e1bd71bf942507fe7
Steven Pennington <steven.pennington@techelevator.com>
2021-03-30 16:11:31 -0400

    added some files and a sidebar

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router/index'
+import store from './store/index'
+import BootStrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+Vue.use(BootStrapVue)
 Vue.config.productionTip = false
 
 new Vue({
+  router,
+  store,
   render: h => h(App),
 }).$mount('#app')
